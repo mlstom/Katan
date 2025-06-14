f3b7a79c2200b71c87b6df490e34464a1d8a551b
Milos Tomic <milostomic502@gmail.com>
2025-06-14 15:53:20 +0200

    reka

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Stage, Layer, Rect, Text } from 'react-konva';
 import './App.css'
 
+
 function App() {
   const [count, setCount] = React.useState(0)
 
   return (
     
-    <><Stage width={400} height={400} >
+    <><Stage width={600} height={600} >
+      <Layer>
+        <Rect  width={600} height={600} fill="lightblue"/>
+      </Layer>
     <Layer>
       <Text text="Hello Canvas!" fontSize={20} x={50} y={50} />
       <Rect  draggable = {true} x={70} y={100} width={400} height={400} fill="lightgreen" />
