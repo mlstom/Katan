73c9b89e439af3bded115038afbf373979ba4473
Milos Tomic <milostomic502@gmail.com>
2025-06-16 15:06:30 +0200

    promena izgleda terena

--- src/classes/Tile.jsx
+++ src/classes/Tile.jsx
@@ -9,7 +9,25 @@ export class Tile {
     this.resource = resource
     this.number = number
     this.lopov = lopov
-    this.polja = polja
+    this.polja = polja 
+    if(resource == "drvo"){
+      this.rektBoja = "#2D4F2B"
+    }
+    if(resource == "pustinja"){
+      this.rektBoja ="#F5F0CD"
+    }
+    if(resource == "ovca"){
+      this.rektBoja = "#B6F500"
+    }
+    if(resource == "cigla"){
+      this.rektBoja = "#FFAAAA"
+    }
+    if(resource=="kamen"){
+      this.rektBoja = "#948979"
+    }
+    if(resource=="zito"){
+      this.rektBoja = "#FED16A"
+    }
   }
 
   render() {
@@ -18,18 +36,24 @@ export class Tile {
         key={this.id}
       >
         <RegularPolygon
-
           x={this.x}
           y={this.y}
           width={50}
           height={50}
-          fill={"red"}
+          fill={this.rektBoja}
           sides={6}
         />
         <ImageComponent
           src={`src/assets/broj${this.number}.png`} // lokalni path, mora da bude validan
           x={this.x}
-          y={this.y}
+          y={this.y+10}
+          width={20}
+          height={20}
+        />
+        <ImageComponent
+          src={`src/assets/${this.resource}.png`} // lokalni path, mora da bude validan
+          x={this.x}
+          y={this.resource!="pustinja" ? this.y-10 : this.y}
           width={20}
           height={20}
         />
